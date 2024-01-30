b06af55cb10c64af29fe5b70c7c1afe78ce86040
Brian Henry <brian@bitpan.com>
2024-01-30 10:45:29 -0600

    add deposit create basics

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,5 +1,5 @@
 class DepositsController < ApplicationController
-  before_action :set_tradeline, only: [:index]
+  before_action :set_tradeline, only: [:index, :create]
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
@@ -10,6 +10,11 @@ class DepositsController < ApplicationController
     render json: Deposit.find(params[:id])
   end
 
+  # 2018-01-09 is AR-accepted format for dates
+  def create
+    @tradeline.deposits << Deposit.new(deposits_params)
+  end
+
   private
 
   def set_tradeline
@@ -18,5 +23,9 @@ class DepositsController < ApplicationController
 
   def not_found
     render json: 'not_found', status: :not_found
+  end
+
+  def deposits_params
+    @deposits_params ||= params.require(:deposit).permit(:deposit_date, :amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :tradelines, only: [:index, :show] do
-    resources :deposits, only: [:index, :show], shallow: true
+    resources :deposits, only: [:index, :show, :create], shallow: true
   end
 end
