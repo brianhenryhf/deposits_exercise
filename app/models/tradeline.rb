6491f88a09fceee92363110b969abf300158ce7b
Brian Henry <brian@bitpan.com>
2024-01-30 09:39:23 -0600

    add tradeline model methods

--- app/models/tradeline.rb
+++ app/models/tradeline.rb
@@ -1,3 +1,13 @@
 class Tradeline < ApplicationRecord
   has_many :deposits
+
+  def outstanding_balance
+    amount - total_deposit_amounts
+  end
+
+  private
+
+  def total_deposit_amounts
+    deposits.pluck(:amount).sum
+  end
 end
